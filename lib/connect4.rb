13d94e35da6cf87ab511bd76996b4ea209788163
Josh Cheek <josh.cheek@gmail.com>
2016-12-02 06:43:59 -0600

    Some refactorings

--- lib/connect4.rb
+++ lib/connect4.rb
@@ -2,15 +2,8 @@ module Connect4
   class Board
     attr_reader :width, :height, :win_length, :tile1, :tile2, :crnt_move
 
-    def initialize(
-        width,
-        height,
-        win_length,
-        tile1,
-        tile2,
-        array = Array.new(width*height),
-        crnt_move = 0
-      )
+    def initialize(width, height, win_length, tile1, tile2,
+                   array=Array.new(width*height), crnt_move=0)
       @width      = width
       @height     = height
       @tile1      = tile1
@@ -30,35 +23,17 @@ module Connect4
       x = column_no
       y += 1 while y < height && !new_array[y*width + column_no]
       y -= 1
-      if y < 0
-        require "pry"
-        binding.pry
-      end
+      raise "Can\'t move into full column" if y < 0
       new_array[y*width + column_no] = current_player
-      self.class.new width, height, win_length, tile1, tile2, new_array, crnt_move+1
-    end
-
-    def to_s
-      str = ""
-      height.times.map do |y|
-        str << "|"
-        width.times.map do |x|
-          element = @array[y*width+x]
-          str << "#{element||' '}|"
-        end
-        str << "\n"
-      end
-      str
+      self.class.new(width, height, win_length, tile1, tile2, new_array, crnt_move+1)
     end
 
     def current_player
-      return tile1 if crnt_move.even?
-      tile2
+      crnt_move.even? ? tile1 : tile2
     end
 
     def next_player
-      return tile2 if crnt_move.odd?
-      tile1
+      crnt_move.odd? ? tile1 : tile2
     end
 
     def over?
@@ -127,56 +102,49 @@ module Connect4
     private
 
     def best_move_and_result(board, indentation=0)
-      # puts board.to_s.gsub(/^/, '  '*indentation)
-      # if @prynow
-      #   require "pry"
-      #   binding.pry
-      #   @prynow=false
-      # end
+      us   = board.current_player
+      them = board.next_player
 
-      # don't try to play on a board that is over
-      me  = board.current_player
-      you = board.next_player
-
-      # require "pry"
-      # binding.pry
-      # play out the game for each available move
+      # play the game for each available move, take it if we win there
       moves_to_boards = board.available_columns.map do |col|
         new_board = board.move(col)
         winner    = new_board.winner
-        return col, :won if winner == me # take the win
+        return col, :won if winner == us # take the win immediately
         [col, new_board]
       end
 
+      # we didn't immediately win, so play the game out to see if we eventually win
       moves = moves_to_boards.map do |col, new_board|
         winner = new_board.winner
-        if winner == you # we lost
-          [col, :lost]
-        else
-          # if we can keep playing on this board, find out what the opponent's result will be
-          move, result = best_move_and_result new_board, indentation+1
+        # we lost
+        next [col, :lost] if winner == them
 
-          # if our opponent lost, we won
-          return col, :won if result == :lost
+        # if we can keep playing on this board, find out what their result will be
+        move, their_result = best_move_and_result new_board, indentation+1
 
-          # otherwise, we either lost or drew
-          [col, result == :won ? :lost : :draw]
-        end
+        # if they lost, we won, take it immediately
+        return col, :won if their_result == :lost
+
+        # we either lost or drew
+        [col, their_result == :won ? :lost : :drew]
       end
 
-      # it is a draw if there are no moves
-      return nil, :draw if moves.empty?
+      # we drew if there are no moves
+      return nil, :drew if moves.empty?
 
       # take the draw if it exists
-      moves.each { |col, result| return col, result if result == :draw }
+      moves.each { |col, result| return col, result if result == :drew }
 
-      # take the loss if it exists
+      # take the loss if it exists (this is kind of dumb b/c it considers all losses equivalent,
+      # since it cannot win if it goes second, it considers all moves equivalent instead of
+      # blocking you when you're about to win)
       moves.each { |col, result| return col, result }
 
       raise "this shouldn't be possible"
     end
   end
 end
+
 
 if $PROGRAM_NAME == __FILE__
   def print_board(board)
@@ -185,22 +153,25 @@ if $PROGRAM_NAME == __FILE__
     red    = "\e[48;5;#{16+6*6*5+6*5}m"
     blue   = "\e[48;5;#{16+5}m"
     black  = "\e[48;5;16m"
-    substitutions = {
-      board.tile1.to_s => "#{red}  #{off}",
-      board.tile2.to_s => "#{yellow}  #{off}",
-      " "              => "#{black}  #{off}",
-      "|"              => "#{blue} #{off}",
-      "\n"             => "\n#{blue}#{' '*(board.width*3+1)}#{off}\n",
+    vsep   = "#{blue} #{off}"
+    border = "#{blue}#{' '*(board.width*3+1)}#{off}"
+    hsep   = "\n#{border}\n"
+    tiles  = {
+      board.tile1 => "#{red}  #{off}",
+      board.tile2 => "#{yellow}  #{off}",
+      nil         => "#{black}  #{off}",
     }
-    puts ("\n#{board}").chars.map { |char| substitutions[char] || char }.join
+    ansi_board = board.to_a.map do |row|
+      vsep + row.map { |tile| tiles[tile] }.join(vsep) + vsep
+    end.join(hsep)
+    puts border, ansi_board, border
   end
 
   board = Connect4::Board.new 4, 4, 3, :a, :b
   until board.over?
-    # robot move
-    move = Connect4::Robot.new(board).best_move
-    break unless move
-    board = board.move move
+    # robot moves first (mostly b/c it plays second kind of stupidly: since it predicts it will lose
+    # it rates every available move as equivalently a loss)
+    board = board.move Connect4::Robot.new(board).best_move
     break if board.over?
 
     # human move
